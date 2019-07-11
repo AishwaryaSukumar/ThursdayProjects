df43528e650f46538520e5cfbfe2b66c4a89e7d2
registered <registered>
2019-07-11 11:29:58 +0530

    add all data

---
+++ src/Org/cap/tester/login/Today.java
@@ -0,0 +1,18 @@
+package Org.cap.tester.login;
+
+public class Today {
+	private static Object case1;
+
+	public static void main(String[] args)
+	{
+		String s="             Particular to the world          ";
+		System.out.println(s);
+boolean b=s.isEmpty();
+System.out.println(b);
+
+
+			}
+	
+	
+}
+
